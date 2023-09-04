77aca577e6d0e07f6d471d5596cdbf0e7b6376fd
이충열 <chancho0518@gmail.com>
2023-09-05 00:28:35 +0900

    Spring Boot SSR 구현 - Thymeleaf 의존성 추가 - Thymeleaf 템플릿 엔진으로 SSR 구현

---
+++ exercise/src/main/java/com/project/exercise/controller/SampleSSRController.java
@@ -0,0 +1,28 @@
+package com.project.exercise.controller;
+
+import com.project.exercise.dto.SampleData;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class SampleSSRController {
+
+    @GetMapping("/sample")
+    public String samplePage(Model model) {
+        List<SampleData> sampleDataList = new ArrayList<>();
+        sampleDataList.add(new SampleData(1, "Sample Item 1"));
+        sampleDataList.add(new SampleData(2, "Sample Item 2"));
+        sampleDataList.add(new SampleData(3, "Sample Item 3"));
+        sampleDataList.add(new SampleData(4, "Sample Item 4"));
+        sampleDataList.add(new SampleData(5, "Sample Item 5"));
+        sampleDataList.add(new SampleData(6, "Sample Item 6"));
+
+        model.addAttribute("dataList", sampleDataList);
+
+        return "samplePage";
+    }
+}
---
+++ exercise/src/main/java/com/project/exercise/dto/SampleData.java
@@ -0,0 +1,24 @@
+package com.project.exercise.dto;
+
+public class SampleData {
+
+    private int id;
+    private  String name;
+
+    public SampleData() {
+    }
+
+    public SampleData(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
