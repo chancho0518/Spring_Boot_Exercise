c46f254dac5417f51f270c23d3e41b465208f0b2
이충열 <chancho0518@gmail.com>
2023-09-12 17:43:27 +0900

    member service 구현

---
+++ member/src/main/java/com/member/service/MemberService.java
@@ -0,0 +1,49 @@
+package com.member.service;
+
+import com.member.domain.Member;
+import com.member.repository.MemberRepository;
+import com.member.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원 가입
+     *
+     */
+    // 같은 이름의 중복 회원가입 X
+    public Long join(Member member) {
+        // 중복 회원 검증
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                        .ifPresent(m -> {
+                            throw new IllegalStateException("이미 존재하는 회원입니다.");
+                        });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+}
